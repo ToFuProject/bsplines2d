2a7d30d14aa63c41d95a4de0d051e674d4c6dbdf
AD265925 <AD265925@sirrah.intra.cea.fr>
2023-06-20 19:56:45 +0200

    [#24] Propagated ref_vector_strategy to plot_as_profile2d()

--- bsplines2d/_class02_BSplines2D.py
+++ bsplines2d/_class02_BSplines2D.py
@@ -941,6 +941,7 @@ class BSplines2D(Previous):
         # ref vectors
         dref_vectorZ=None,
         dref_vectorU=None,
+        ref_vector_strategy=None,
         # interpolation
         val_out=None,
         nan0=None,
@@ -972,6 +973,7 @@ class BSplines2D(Previous):
             # ref vectors
             dref_vectorZ=dref_vectorZ,
             dref_vectorU=dref_vectorU,
+            ref_vector_strategy=ref_vector_strategy,
             # interpolation
             val_out=val_out,
             nan0=nan0,
